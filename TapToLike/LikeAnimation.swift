5b42f352b1df762c94bb7284cfb2d0af4ad63b23
Abrahamas <babyyuguioh@gmail.com>
2019-02-09 21:00:14 -0500

    Done

---
+++ TapToLike/LikeAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  LikeAnimation.swift
+//  TapToLike
+//
+//  Created by Abrahamas on 11/20/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import Foundation
---
+++ TapToLike/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TapToLike
+//
+//  Created by Abrahamas on 11/20/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
